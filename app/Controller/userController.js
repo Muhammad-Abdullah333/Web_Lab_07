a213e2fdfd40ce75796cc4b62c11c87a802adbc5
Muhammad Abdullah <writetomuhammadabdullah@gmail.com>
2023-03-22 13:21:07 +0500

    JWT_Token

--- app/Controller/userController.js
+++ app/Controller/userController.js
@@ -1,13 +1,16 @@
 const userModel = require("../Models/userSchema");
+const jwt = require("jsonwebtoken");
 
 let signup = (req, res) => {
     let user = new userModel({
         username: req.body.username,
         password: req.body.password,
     });
+    console.log(user);
     user.save().then((user) => {
         res.status(200).json({ message: "User created successfully!", user: user });
     }).catch((err) => {
+        console.log(err);
         res.status(500).json({ message: "Error creating user!", error: err });
     });
 }
@@ -16,7 +19,8 @@ let login = (req, res) => {
     userModel.findOne({ username: req.body.username }).then((user) => {
         if (user) {
             if (user.password == req.body.password) {
-                res.status(200).json({ message: "User logged in successfully!", user: user });
+                let token = jwt.sign({ id: user._id ,role: user.role}, process.env.SECRET_KEY, { expiresIn: "24h" }); // web token for authentication in multiple requests
+                res.status(200).json({ message: "User logged in successfully!", user: user, token: token });
             } else {
                 res.status(500).json({ message: "Incorrect password!" });
             }
@@ -28,12 +32,31 @@ let login = (req, res) => {
     });
 }
 
-let asd = (req, res) => {
-    res.send("Hello Meow User!");
+let DecodedUser = (req,res ,next) => {
+    let token = req.headers['token']
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            res.status(500).json({ message: "Error verifying token!", error: err });
+        } else {
+            req.decoded = decoded;
+            next();
+        }
+    });
 }
+
+let isAdmin = (req,res,next) => {
+    if(req.decoded.role == "admin"){
+        next();
+    }else{
+        res.status(403).json({ message: "You are not an admin!" });
+    }
+}
+
 
 module.exports = {
     signup,
-    login   
+    login,
+    DecodedUser,
+    isAdmin
 };
 
--- app/Routes/userRoutes.js
+++ app/Routes/userRoutes.js
@@ -1,7 +1,14 @@
 const userRoutes = require("express").Router();
-const {signup,login} = require("../Controller/userController");
+const {signup,login, DecodedUser, isAdmin} = require("../Controller/userController");
+const jwt = require("jsonwebtoken");
+
+
 
 userRoutes.post("/signup",signup);
 userRoutes.post("/login",login);
+userRoutes.get("/profile",DecodedUser,isAdmin,(req,res) => {
+    res.status(200).json({ message: "User profile!", user: req.decoded });
+});
+
 
 module.exports = userRoutes;
